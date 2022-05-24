58fcc310ca7c21fdd2123789f5a308e62df152da
Prashanth Mohan <prashmohanuon@gmail.com>
2022-05-24 16:04:47 +0200

    67. creating a form in the template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
 import { StarWarService } from './star-wars.service';
 import { LogService } from './log.service';
+import { CreateCharacterComponent } from './create-character/create-character.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabsComponent,
     ListComponent,
-    ItemComponent
+    ItemComponent,
+    CreateCharacterComponent
   ],
   imports: [
     BrowserModule,
